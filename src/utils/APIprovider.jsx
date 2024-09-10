553aca08838f36b3ed58a44260f038eb40955065
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-09-10 22:38:19 +0200

    Update APIprovider.jsx
    test mit dem localhost 5000

--- src/utils/APIprovider.jsx
+++ src/utils/APIprovider.jsx
@@ -4,7 +4,8 @@ const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
   //const apiBaseUrl = "http://192.168.178.177:3000";
-  const apiBaseUrl = "http://localhost:3000";
+  //const apiBaseUrl = "http://localhost:5000";
+  const apiBaseUrl = "http://localhost:5000";
 
   return (
     <ApiContext.Provider value={apiBaseUrl}>{children}</ApiContext.Provider>
