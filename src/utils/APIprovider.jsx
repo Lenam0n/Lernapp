d1aed49a475814c4dec5cacacbd0011c2dc372d9
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-09-11 01:28:32 +0200

    Update APIprovider.jsx

--- src/utils/APIprovider.jsx
+++ src/utils/APIprovider.jsx
@@ -5,7 +5,8 @@ const ApiContext = createContext();
 export const ApiProvider = ({ children }) => {
   //const apiBaseUrl = "http://192.168.178.177:3000";
   //const apiBaseUrl = "http://localhost:5000";
-  const apiBaseUrl = "http://backend:5000";
+  //const apiBaseUrl = "http://backend:5000";
+  const apiBaseUrl = "http://lumiquest_backend.lenam0n.net";
 
   return (
     <ApiContext.Provider value={apiBaseUrl}>{children}</ApiContext.Provider>
