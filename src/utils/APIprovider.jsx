b8cdfa76e5636f0b50ed4233661c23c9da84b659
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-09-11 03:14:25 +0200

    Update APIprovider.jsx

--- src/utils/APIprovider.jsx
+++ src/utils/APIprovider.jsx
@@ -5,8 +5,8 @@ const ApiContext = createContext();
 export const ApiProvider = ({ children }) => {
   //const apiBaseUrl = "http://192.168.178.177:3000";
   //const apiBaseUrl = "http://localhost:5000";
-  //const apiBaseUrl = "http://backend:5000";
-  const apiBaseUrl = "http://lumiquestbackend.lenam0n.net";
+  const apiBaseUrl = "http://lernapp_backend:5000";
+  //const apiBaseUrl = "http://lumiquestbackend.lenam0n.net";
 
   return (
     <ApiContext.Provider value={apiBaseUrl}>{children}</ApiContext.Provider>
